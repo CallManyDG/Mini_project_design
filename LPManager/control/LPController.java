77b20e06ce81e96fa89794ece910cf74155046c3
Dan Vyhlidal <dan.vyhlidal@seznam.cz>
2020-10-28 12:56:38 +0100

    Added finder for copies
    Added finder for copies in LPController

--- LPManager/control/LPController.java
+++ LPManager/control/LPController.java
@@ -1,6 +1,36 @@
 package control;
-
+import java.util.*;
+import model.*;
 public class LPController
 {
+    public Copy findFirstAvailableCopy(String title, String author) {
+        // Find LP by name and author
+        LP lp = getLPByNameAndAuthor(title, author);
+        // Loop through all the copies
+        Iterator<Copy> copies = lp.getAllCopies().iterator();
+        
+        while(copies.hasNext()){
+            Copy copy = copies.next();
+            // return the first available copy that has been found
+            if(copy.isAvailable())
+                return copy;
+        }
+        return null;
+            
+    }
     
+    private LP getLPByNameAndAuthor(String title, String author) {
+        Iterator<LP> lps = LPContainer.getInstance().getAllLPs().iterator();
+        
+        while(lps.hasNext()) {
+            LP lp = lps.next();
+            
+            if(lp.getTitle().equals(title) && lp.getAuthor().equals(author)){
+                return lp;
+            }
+        }
+        
+        return null;
+            
+    }
 }
